0db8ae25bf8bc45ec2591c734875e806bbfabd13
Hedgeken95 <ken10951128@icloud.com>
2020-10-21 20:15:19 +0900

    本の投稿

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,7 +10,7 @@ class CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
-    if @category.save
+    if @category.save!
       redirect_to root_path
     else
       render :new
